bf9daabbfa3a468271dce9d91b9c2a6c47f05da2
bjk95 <bradley.kester@barcelonagse.eu>
2022-10-16 17:49:40 +1100

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = exports.multiply = void 0;
+function multiply(a, b) {
+  return a * b;
+}
+exports.multiply = multiply;
+function add(a, b) {
+  return a * b;
+}
+exports.add = add;
